854619ffe7cbf73efc173963ad22a77bf4ac3491
Yogesh Nagar <yogesh.nagar1000@gmail.com>
2022-07-20 16:37:32 +0100

    Integer to Roman

--- src/test/java/com/example/roman/RomanConversionTest.java
+++ src/test/java/com/example/roman/RomanConversionTest.java
@@ -1,4 +1,18 @@
 package com.example.roman;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
 public class RomanConversionTest {
+
+    @Test
+    public void testIntegerToRoman_ConversionOfSingleDigit() {
+        String result = null;
+        assertEquals("I", result);
+    }
+
 }
